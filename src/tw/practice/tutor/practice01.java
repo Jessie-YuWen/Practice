1d4beaf05842af1449f4c0ae23b4a5128378d4ee
Jessie-YuWen <jk1765688@gmail.com>
2024-11-25 22:41:15 +0800

    九九乘法表，新增控制項

--- src/tw/practice/tutor/practice01.java
+++ src/tw/practice/tutor/practice01.java
@@ -3,17 +3,24 @@ package tw.practice.tutor;
 public class practice01 {
 
 	public static void main(String[] args) {
+		int row = 3; // 列數(圈數)
+		int col = 3; // 行數
+		int st = 1; // 從幾開始
 
-		for (int j = 1; j <= 9; j++) {
+		for (int k = 0; k < row; k++) {
 
-			for (int i = 1; i < 10; i++) {
+			for (int j = 1; j <= 9; j++) {
 
-				int r = i * j;
+				for (int i = st; i < st + col; i++) {
 
-				System.out.printf(" %d x %d = %d\t", i, j, r);
+					int newi = i + col * k;
+					int r = newi * j;
+
+					System.out.printf(" %d x %d = %d\t", newi, j, r);
+				}
+				System.out.println();
 			}
-			System.out.println();
-
+			System.out.println("---");
 		}
 	}
 }
